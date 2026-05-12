9b225ef73c8e5b3fc09f93decd1c162d83213679
marcomisseroni <marco.misseroni@studenti.unitn.it>
2026-05-12 10:06:44 +0200

    feat: add custom tracker

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
 from ultralytics import YOLO
 import cv2
 
+def track():
+    model = YOLO('yolo26n.pt')
+    results = model.track(
+        source='videos/vid_test.mp4', 
+        tracker='botsort.yaml', 
+        conf=0.1, 
+        iou=0.7,
+        show=True, 
+        stream=True)
+    
+    for r in results:
+        pass
+
+
 def main():
-    print('test')
+    track()
 
 if __name__ == '__main__':
     main()
